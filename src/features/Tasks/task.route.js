7c3dd59a6d133dd905212da45b6eb34c6d1934e4
VivekJawale <vivekjawale41@gmail.com>
2023-03-04 12:55:53 +0530

    added

--- src/features/Tasks/task.route.js
+++ src/features/Tasks/task.route.js
@@ -1,8 +1,34 @@
+const jwt = require("jsonwebtoken");
 const express = require("express");
-const Task = require("./task.model");
-const app = express();
+const SECRET_KEY = process.env.SECRET_KEY;
+const User = require("../features/Auth/user.model")
 
 
+const authMiddleWare = async (req, res, next) => {
+    const { token } = req.headers;
+    try {
+        if (!token) {
+            return res.send("Token missing");
+        } else {
+            let verify = jwt.verify(token, SECRET_KEY);
+            if (!verify) {
+                return res.send("Invalid token");
+            } else {
+                let user = await User.findOne({ _id: verify._id });
+                if (!user) {
+                    return res.send("user not found");
+                } else {
+                    req._id = user._id;
+                    next();
+                }
+            }
+        }
+    } catch (error) {
+        return res.send(error);
+    }
+};
+
+app.use(authMiddleWare);
 app.get("", async (req, res) => {
     try {
         let task = await Task.find({ user: req._id }).populate(["user"]);
